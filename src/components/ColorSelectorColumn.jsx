3538e8aac875810788cc1a995fcc5863fb64d7f2
Noel Abellera <noel.abellera@gmail.com>
2018-03-16 23:53:20 -0700

    Add color selector column component

---
+++ src/components/ColorSelectorColumn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ColorSelectorColumn = (props) => {
+    return(
+        <div>
+            this is the color selector column
+        </div>
+    )
+};
+
+export default ColorSelectorColumn;
