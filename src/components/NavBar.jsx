a515b04ef66276f7966746d17ff04a38aef93052
Noel Abellera <noel.abellera@gmail.com>
2018-03-27 20:24:55 -0700

    style links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => {
     return (
         <div className="Navbar">
-            <h2><Link to='/'>Home  </Link> |
-            <Link to='/about'> About</Link></h2>
+            <h2><Link className="Links" to='/'>Home  </Link> |
+            <Link className="Links" to='/about'> About</Link></h2>
         </div>
     )
 }
